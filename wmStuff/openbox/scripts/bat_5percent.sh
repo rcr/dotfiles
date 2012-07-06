b4d70a81d06928d1812db23b650bb5f01b5b65a6
Richard Robbins <mail@robbinsr.com>
2012-07-06 00:49:47 -0400

    Dont need openbox/awesome stuff anymore

--- wmStuff/openbox/scripts/bat_5percent.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-alarm=$(cat /sys/class/power_supply/BAT0/alarm)
-
-A=$(echo "scale=2; `cat /sys/class/power_supply/BAT0/charge_now` / `cat /sys/class/power_supply/BAT0/charge_full`" | bc | cut -c 2-3)
-if (($A < 79)); then
-	notify-send "Warning:" "Battery charge: $A" --icon=/usr/share/icons/oxygen/48x48/status/task-attention.png
-fi
--- wmStuff/openbox/scripts/f4.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-notify-send "Error" "No script set" --expire-time=3000  --icon=/usr/share/icons/oxygen/48x48/status/task-attempt.png
