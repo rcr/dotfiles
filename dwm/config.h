d3051342f634e9961aa4db44c5500b2cdcceb7a7
Richard Robbins <mail@robbinsr.com>
2012-07-07 22:10:02 -0400

    script changes for wireless internet

--- dwm/config.h
+++ dwm/config.h
@@ -15,7 +15,7 @@ static const char urgbgcolor[]      = "#c90000";
 static const char font[]            = "-*-clean-medium-r-*-*-12-*-*-*-*-*-*-*";
 
 static const unsigned int borderpx  = 1;
-static const unsigned int snap      = 8;
+static const unsigned int snap      = 25;
 static const Bool showbar           = True;
 static const Bool topbar            = True; 
 
@@ -59,30 +59,30 @@ static Key keys[] = {
   { MODKEY,                       XK_b,      togglebar,      {0} },
   { MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
   { MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-//  { MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-//  { MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
   { MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
   { MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
   { MODKEY,                       XK_Return, zoom,           {0} },
   { MODKEY,                       XK_Tab,    view,           {0} },
   { MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-//  { MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-//  { MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-//  { MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-//  { MODKEY,                       XK_space,  setlayout,      {0} },
-//  { MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
   { MODKEY,                       XK_0,      view,           {.ui = ~(1 << 1) } },
-//  { MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-//  { MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-//  { MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-//  { MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-//  { MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
   TAGKEYS(                        XK_1,                      0)
   TAGKEYS(                        XK_2,                      1)
   TAGKEYS(                        XK_3,                      2)
   TAGKEYS(                        XK_4,                      3)
   TAGKEYS(                        XK_5,                      4)
   { MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+//  { MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
+//  { MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+//  { MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
+//  { MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
+//  { MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+//  { MODKEY,                       XK_space,  setlayout,      {0} },
+//  { MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+//  { MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+//  { MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
+//  { MODKEY,                       XK_period, focusmon,       {.i = +1 } },
+//  { MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
+//  { MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 };
 
 /* click can be ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
